2c233e13fbd9617ebc51b9df2e57b5750e9bb176
Gabstaudt <macstaudt@gmail.com>
2024-11-18 21:06:31 -0300

    teste 2

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo "Teste simples. Backend está ativo!";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
+echo json_encode(["mensagem" => "Bem-vindo ao backend!"]);
 ?>
